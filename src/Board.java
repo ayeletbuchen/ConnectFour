20e50e5c9328acfa1d07cd99af5132b81ec1219f
Ayelet <ayeletcb@gmail.com>
2019-04-08 18:44:49 -0400

    began code to check if game is over

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,7 @@ import java.awt.*;
 
 public class Board extends JComponent {
 
+    private final int winningNumber = 4;
     private final int rows = 6;
     private final int cols = 7;
     private final int space = 25;
@@ -61,23 +62,110 @@ public class Board extends JComponent {
             return;
         }
 
+        int row = findRow(col);
+        changePieceColor(pieces[row][col]);
+        Color player = pieces[row][col].getColor();
+
+        black = !black;
+        firstPaint = false;
+        repaint(pieces[row][col].getXPosition(), pieces[row][col].getYPosition(), pieceSize, pieceSize);
+        if (isGameOver(player, row, col)) {
+            System.out.println((player == Color.BLACK ? "Black" : "Red") + " wins!");
+            // TODO turn above statement into dialgoe box and make users unable to continue here
+        }
+    }
+
+    private int findRow(int col) {
         int row = 0;
         for (int i = 0; i < rows && pieces[i][col].getColor() == defaultColor; i++) {
             row = i;
         }
 
-        Piece piece = pieces[row][col];
+        return row;
+    }
 
+    private void changePieceColor(Piece piece) {
         if (black) {
             piece.setColor(Color.BLACK);
         }
         else {
             piece.setColor(Color.RED);
         }
+    }
 
-        black = !black;
-        firstPaint = false;
+    private boolean isGameOver(Color player, int row, int col) {
+        if (isVerticalWin(player, row, col)
+                || isHorizontalWin(player, row, col)
+                || isDownwardDiagonalWin(player, row, col)
+                || isUpwardDiagonalWin(player, row, col)) {
+            return true;
+        }
+        return false;
+    }
 
-        repaint(piece.getXPosition(),piece.getYPosition(), pieceSize, pieceSize);
+    private boolean isVerticalWin(Color player, int row, int col) {
+        if (row < 2) {
+            return false;
+        }
+
+        int matchingPieces = 1;
+        for (int i = row + 1; i <= rows && pieces[i][col].getColor() == player; i++) {
+            matchingPieces++;
+            if (matchingPieces == winningNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isHorizontalWin(Color player, int row, int col) {
+        int matchingPieces = 1;
+        for (int i = col - 1; i >= 0 && pieces[row][i].getColor() == player; i--) {
+            matchingPieces++;
+        }
+
+        for (int i = col + 1; i < cols && pieces[row][i].getColor() == player; i++) {
+            matchingPieces++;
+        }
+
+        if (matchingPieces >= winningNumber) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDownwardDiagonalWin(Color player, int row, int col) {
+        int matchingPieces = 1;
+        for (int i = row + 1, j = col + 1; i < rows && j < cols && pieces[i][j].getColor() == player; i++, j++) {
+            matchingPieces++;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0 && pieces[i][j].getColor() == player; i--, j--) {
+            matchingPieces++;
+        }
+
+        if (matchingPieces >= winningNumber) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isUpwardDiagonalWin(Color player, int row, int col) {
+        int matchingPieces = 1;
+
+        for (int i = row - 1, j = col + 1; i >= 0 && col < cols && pieces[i][j].getColor() == player; i--, j++) {
+            matchingPieces++;
+        }
+
+        for (int i = row + 1, j = col - 1; i < rows && j >= 0 && pieces[i][j].getColor() == player; i++, j--) {
+            matchingPieces++;
+        }
+
+        if (matchingPieces >= winningNumber) {
+            return true;
+        }
+
+        return false;
     }
 }
